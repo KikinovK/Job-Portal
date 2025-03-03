a130ba47d06754786d073875f4606f05b6564161
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-03-03 23:04:09 +0200

    feat: add translate DisplayJobs

--- pages/frontend/postedJob.jsx
+++ pages/frontend/postedJob.jsx
@@ -8,15 +8,20 @@ import React, { useEffect, useState } from 'react'
 import { InfinitySpin } from 'react-loader-spinner';
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify';
-import useSWR from 'swr'
+import useSWR from 'swr';
+
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export default function PostedJobs() {
     const router = useRouter();
     const dispatch = useDispatch();
     const user = useSelector(state => state?.User?.userData)
     const myJobs = useSelector(state => state?.Job?.myJobs);
-    const id = user?._id
+    const id = user?._id;
 
+    const { locale } = router;
+    const { t } = useTranslation('postedJob');
 
 
     useEffect(() => {
@@ -26,8 +31,8 @@ export default function PostedJobs() {
     }, [user, id, Cookies])
 
 
-    const { data, error, isLoading } = useSWR('/getMyPostedJobs', () =>  get_my_posted_job(id))
-    
+    const { data, error, isLoading } = useSWR('/getMyPostedJobs', () =>  get_my_posted_job(id, locale))
+
     useEffect(() => {
         if(data) dispatch(setMyJobs(data?.data))
     }, [data , dispatch])
@@ -41,14 +46,14 @@ export default function PostedJobs() {
 
                     <div className='bg-gray w-full h-screen flex items-center flex-col justify-center'>
                         <InfinitySpin width='200' color="#4f46e5" />
-                        <p className='text-xs uppercase'>Loading Resources Hold Tight...</p>
+                        <p className='text-xs uppercase'>{t('common:loading')}</p>
                     </div>
                 ) : (
                     <>
                         <NavBar />
                         <div className='w-full  pt-20'>
                             <div className='w-full h-20 bg-gray-50 text-indigo-600 font-bold flex items-center justify-center flex-col'>
-                                <h1 className='text-3xl'>Posted Jobs</h1>
+                                <h1 className='text-3xl'>{t('title')}</h1>
                             </div>
                             <div className='w-full h-full px-4 py-4 flex  overflow-y-auto  items-start justify-center flex-wrap'>
                                 {
@@ -64,3 +69,11 @@ export default function PostedJobs() {
         </>
     )
 }
+
+export async function getStaticProps({ locale }) {
+    return {
+      props: {
+        ...(await serverSideTranslations(locale, ['common', 'navbar', 'postedJob', 'jobsCard'])),
+      },
+    };
+  }
