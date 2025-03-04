15c00e8c711d9c06e6a458f078380c6a4e67a986
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-03-04 13:55:34 +0200

    feat: add translation getApplicationDetail api

--- pages/api/job/getApplicationDetail.js
+++ pages/api/job/getApplicationDetail.js
@@ -2,9 +2,19 @@ import ConnectDB from '@/DB/connectDB';
 import validateToken from '@/middleware/tokenValidation';
 import AppliedJob from '@/models/ApplyJob';
 
+import i18next from '@/i18n_backend';
+
 
 export default async (req, res) => {
     await ConnectDB();
+
+    const lng = req.headers['accept-language'] || (
+        Array.isArray(i18next.options.fallbackLng)
+            ? i18next.options.fallbackLng[0]
+            : i18next.options.fallbackLng
+    );
+    i18next.changeLanguage(lng);
+
     const { method } = req;
     switch (method) {
         case 'GET':
@@ -13,7 +23,7 @@ export default async (req, res) => {
 
             break;
         default:
-            res.status(400).json({ success: false, message: 'Invalid Request' });
+            res.status(400).json({ success: false, message: i18next.t('error_request') });
     }
 }
 
@@ -21,15 +31,15 @@ export default async (req, res) => {
 
 const getApplicationDetail = async (req, res) => {
     const data = req.query;
-    const id = data?.id
+    const id = data?.id;
 
-    if (!id) return res.status(400).json({ success: false, message: "Please Login" })
+    if (!id) return res.status(400).json({ success: false, message: i18next.t('error_logirn') })
 
     try {
         const getApplicationDetails = await AppliedJob.findById(id).populate('job').populate('user')
         return res.status(200).json({ success: true, data: getApplicationDetails })
     } catch (error) {
         console.log('Error in getting a specifed Job job (server) => ', error);
-        return res.status(403).json({ success: false, message: "Something Went Wrong Please Retry login  !" })
+        return res.status(403).json({ success: false, message: i18next.t('error_retry_login') })
     }
 }
