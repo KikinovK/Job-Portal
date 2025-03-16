edd8f4b77ba2c49c0b01825b8739f502f3da6f9f
Kostaintyn Kikinov <kikinov.k@gmail.com>
2025-03-16 12:24:22 +0200

    fix: remove clear user

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import '@/styles/globals.css'
 import { store } from '@/Store/store'
 import { Provider } from 'react-redux'
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Cookies from 'js-cookie';
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
+// import Cookies from 'js-cookie';
 import 'react-toastify/dist/ReactToastify.css';
 import { appWithTranslation } from 'next-i18next';
 
@@ -15,21 +15,22 @@ function App({
   pageProps: { session, ...pageProps },
 })
 {
-  const router = useRouter();
+  // for testing
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleBeforeUnload = (event) => {
-      event.preventDefault();
-      localStorage.removeItem("user")
-      Cookies.remove('token')
-    };
+  // useEffect(() => {
+  //   const handleBeforeUnload = (event) => {
+  //     event.preventDefault();
+  //     localStorage.removeItem("user")
+  //     Cookies.remove('token')
+  //   };
 
-    window.addEventListener("beforeunload", handleBeforeUnload);
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
 
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, [router]);
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, [router]);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
